7f1dd3697b3d4c466c722f1e8e19bd02f49b65e9
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-01-13 10:58:12 +0200

    Add getters and setters for properties

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,9 +9,41 @@
 
 class RangeValidator {
   constructor(_from, _to) {
-    this._from = from;
-    this._to = to;
+    this.from = _from;
+    this.to = _to;
 
   }
 
+  get from () {
+    return this._from;
+  }
+
+  get to () {
+    return this._to;
+  }
+
+  set from (value) {
+    if(typeof value !== 'number') {
+      throw new TypeError('from parameter must be numeric');
+    }
+
+    if (typeof this._to !== 'undefined' && value > this._to) {
+      throw new RangeError(`From value must be smaller than to (${this._to})`);
+    }
+
+    this._from = value;
+  }
+
+  set to (value) {
+    if(typeof value !== 'number') {
+      throw new TypeError('from parameter must be numeric');
+    }
+
+    if (typeof this._from !== 'undefined' && this._from > value) {
+      throw new RangeError(`To value must be larger than from (${this._from})`);
+    }
+    this._to = value;
+  }
 }
+
+const data = new RangeValidator(1,10);
