b13ba07a0f9f98708fce3ce5fcdb1932201034b8
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-01-13 11:53:45 +0200

    Add documentation

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,21 +7,39 @@
 // Реализовать getter range, который будет возвращать массив с двумя числами диапазона
 // Реализовать метод validate, который будет принимать число и проверить входит ли число в указанный диапазон.
 
+/** Class for validating numbers in specified range. */
 class RangeValidator {
+  /**
+   * Create a range.
+   * @param {number} _from - min number in range.
+   * @param {number} _to  - max number in range.
+   */
   constructor(_from, _to) {
     this.from = _from;
     this.to = _to;
 
   }
 
+  /**
+   * Get _from value
+   * @returns {number} 
+   */
   get from () {
     return this._from;
   }
 
+  /**
+   * Get _to value
+   * @returns {number} 
+   */
   get to () {
     return this._to;
   }
 
+  /**
+   * Set _from value
+   * @param {number} _from
+   */
   set from (value) {
     if(typeof value !== 'number') {
       throw new TypeError('from parameter must be numeric');
@@ -34,6 +52,10 @@ class RangeValidator {
     this._from = value;
   }
 
+  /**
+   * Set _to value
+   * @param {number} _to
+   */
   set to (value) {
     if(typeof value !== 'number') {
       throw new TypeError('to parameter must be numeric');
@@ -45,10 +67,19 @@ class RangeValidator {
     this._to = value;
   }
 
+  /**
+   * Get range array
+   * @returns {Array} _from and _to values as an array
+   */
   get range () {
     return [this._from, this._to];
   }
 
+  /**
+   * Check if number is in range from _from to _to
+   * @param {number} number 
+   * @returns {(number|RangeError)} returns number if it is in range between _from and _to, else returns RangeError
+   */
   validate(number) {
     if(typeof number !== 'number') {
       throw new TypeError('Inputted parameter must be numeric');
