64eb4643018a31f5cd5510ed4645adc8df5c26f0
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-01-13 10:39:10 +0200

    Add RangeValudator class

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Реализовать класс RangeValidator, со следующими свойствами:
+//     ■  from (number);
+//     ■  to (number);
+// Реализовать getter'ы и setter'ы для обоих свойств
+// Реализовать getter range, который будет возвращать массив с двумя числами диапазона
+// Реализовать метод validate, который будет принимать число и проверить входит ли число в указанный диапазон.
+
+class RangeValidator {
+  constructor(_from, _to) {
+    this._from = from;
+    this._to = to;
+
+  }
+
+}
